3f8c04f31c4d5bbd14297bea7e709e8abfefb762
Cody Taylor <codemister99@yahoo.com>
2016-04-25 01:26:18 -0400

    Add application URL generator.

---
+++ test/lib.createUrl.js
@@ -0,0 +1,17 @@
+var test = require('tape');
+var qs = require('querystring');
+var createUrl = require('../lib/createUrl.js');
+
+// the permissions is the only thing that varies in createUrl at the moment
+function parse(url) {
+  var right = url.split('?')[1];
+  return qs.parse(right).permissions;
+}
+
+test('createUrl', function t1(t) {
+  t.equal(parse(createUrl(5)), '5', 'single number argument');
+  t.equal(parse(createUrl(1, 2)), '3', 'multiple number arguments');
+  t.equal(parse(createUrl([2, 4])), '6', 'single array argument');
+  t.ok(parse(createUrl()), 'no arguments, return default value');
+  t.end();
+});
