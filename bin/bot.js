924cb7fcf1f3f2503e81245b5705b345129bda85
Cody Taylor <codemister99@yahoo.com>
2016-04-27 02:42:43 -0400

    Tested command structure.
    New commands:
     - @blitzbot add <screenname>
     - @blitzbot wr
    
    Nothing set in stone. Expect breaking changes moving forward.

--- bin/bot.js
+++ bin/bot.js
@@ -13,25 +13,145 @@ var db = new Datastore({
   timestampData: true,
 });
 
+var commands = {};
+
+commands.add = {
+  'args': 1,
+  'description': 'Associate your blitz screenname with discord.',
+  'fn': function(msg, screenname, cb) {
+    screenname = screenname.toLowerCase();
+
+    wotblitz.players.list(screenname, null, function(pErr, players) {
+      if (pErr) return cb(pErr);
+
+      var player = players.find(function(p) { return p.nickname.toLowerCase() === screenname; });
+
+      if (player) {
+        client.reply(msg, 'Welcome! You now have access to all commands. :)', {}, function(rErr, sent) {
+          if (rErr) return cb(rErr);
+
+          console.log('sent msg: ' + sent);
+          cb(null, player);
+        });
+      } else {
+        // this could be an error, but for now avoid the noise
+        cb(null);
+      }
+    });
+  },
+  'passRecord': false,
+  'signatures': ['@blitzbot add <screenname>'],
+};
+
+commands.wr = {
+  'args': 0,
+  'description': 'Get the win rate of your account.',
+  'fn': function(msg, record, cb) {
+    wotblitz.players.info([record.account_id], [], ['statistics.all.battles', 'statistics.all.wins'], null, function(iErr, info) {
+      if (iErr) return cb(iErr);
+
+      var wins = helpers.getFieldByPath(info[record.account_id], 'statistics.all.wins');
+      var battles = helpers.getFieldByPath(info[record.account_id], 'statistics.all.battles');
+      var percent = (wins / battles) * 100;
+
+      client.reply(msg, 'You have won ' + wins + ' of ' + battles + '. That is ' + percent.toFixed(2) + '% victory!', {}, function(rErr, sent) {
+        if (rErr) return cb(rErr);
+
+        console.log('sent msg: ' + sent);
+
+        record.wins = wins;
+        record.battles = battles;
+
+        cb(null, record);
+      });
+    });
+  },
+  'passRecord': true,
+  'signatures': ['@blitzbot wr'],
+};
+
+commands.help = {
+  'args': 1,
+  'description': 'List all known commands or get help for a particular command.',
+  'fn': null,
+  'passRecord': false,
+  'signatures': [
+    '@blitzbot help [command]',
+    '(direct message): help [command]',
+  ],
+};
+
+// done last so that the help function can read the entire "commands" structure
+commands.help.fn = function(msg, helpFor, cb) {
+  client.sendMessage(msg.author, 'Help is not yet implemented.', {}, function(err, sent) {
+    if (err) return cb(err);
+
+    cb(null);
+  });
+};
+  
+
 client.on('ready', function() {
   console.log('blitzbot ready!');
   console.log('===============');
 });
 
 client.on('message', function(message) {
-  if (!message.isMentioned(client.user)) return;
+  // Bot will only respond in a DM or when mentioned.
+  if (!message.channel.isPrivate && !message.isMentioned(client.user)) return;
+  if (message.author.id === client.user.id) return;
 
-  var text = message.cleanContent;
   var userId = message.author.id;
+  var text = message.cleanContent.split(' ');
+  var i = text.findIndex(function(e, i, a) {
+    return (message.channel.isPrivate && e === 'help') || (i > 0 && a[i - 1] === '@' + client.user.username);
+  });
+  var command = text[i];
+  var args = text.slice(i + 1);
 
   async.auto({
     record: function(cb) { db.findOne({_id: userId}, cb); },
-    defaultMsg: ['record', function(cb, d) {
-      if (d.record) return cb(null);
-      client.reply(message, "I don't know who you are! Do `@blitzbot add <blitz-username>` first.", {}, cb);
+    runCmd: ['record', function(cb, d) {
+      if (!(command in commands)) return cb(null);
+
+      console.log(userId + ' -- running command: "' + command + '"');
+
+      var obj = commands[command];
+      var arr = [message];
+
+      if (obj.passRecord) {
+        // commands require a saved 'account_id'.
+        if (d.record && d.record.account_id) {
+          arr.push(d.record);
+        } else {
+          return client.reply(message, "I don't know who you are! Do `@blitzbot add <screenname>` first.", {}, function(aErr) {
+            if (aErr) return cb(aErr);
+
+            cb(null);
+          });
+        }
+      }
+
+      if (obj.args > 0) Array.prototype.push.apply(arr, args.slice(0, obj.args));
+      arr.push(cb);
+
+      obj.fn.apply(null, arr);
+    }],
+    update: ['runCmd', function(cb, d) {
+      if (!d.runCmd) return cb(null);
+
+      console.log(userId + ' -- update document');
+
+      var newRecord = d.runCmd;
+      newRecord._id = userId;
+      delete newRecord.updatedAt;
+
+      db.update({_id: userId}, newRecord, {upsert: true}, cb);
     }],
   }, function(err, data) {
     if (err) return console.error(helpers.getFieldByPath(err, 'response.error.text') || err.stack || err);
+
+    console.log(userId + ' -- done');
   });
 });
 
