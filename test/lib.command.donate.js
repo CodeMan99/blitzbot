60d554494eb63ef712ab025e1de1d9a78b52b592
Cody Taylor <codemister99@yahoo.com>
2016-06-18 16:43:17 -0400

    Add donate command.

---
+++ test/lib.command.donate.js
@@ -0,0 +1,45 @@
+var test = require('tape');
+var mockery = require('mockery');
+
+mockery.registerAllowable('./index.js');
+mockery.registerAllowable('../lib/command/donate.js');
+mockery.registerMock('../../wotblitz.json', {
+  paypal: {
+    me: 'https://paypal.me/CodeMan99',
+  },
+});
+mockery.enable();
+
+var donate = require('../lib/command/donate.js');
+
+mockery.disable();
+mockery.deregisterAll();
+
+var callDonate = donate.fn.bind({
+  client: {
+    reply: (message, text) => Promise.resolve(`@${message.author}, ${text}`),
+  },
+});
+
+test('donate', (t) => {
+  t.deepEqual(donate.fn.options, {
+    argCount: 0,
+    argSplit: ' ',
+    description: 'Get a link to generously donate via paypal.',
+    passRecord: false,
+    signatures: ['@BOTNAME donate'],
+  }, 'verify options');
+
+  t.equal(donate.name, 'donate', 'verify Commands method name');
+
+  t.test('command', (st) => {
+    callDonate({author: 'NiceDude [CL]'}).then(result => {
+      var link = result.sentMsg.split(' ').slice(-1)[0];
+
+      st.equal(link, 'https://paypal.me/CodeMan99', 'link is at the end of reply');
+      st.end();
+    }, error => { st.fail(error); st.end(); });
+  });
+
+  t.end();
+});
