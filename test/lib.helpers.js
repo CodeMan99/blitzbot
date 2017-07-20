274581473735212087a6c45ed2122c672966f4e9
Cody Taylor <codemister99@yahoo.com>
2017-07-20 14:10:12 -0400

    Reference to getFieldByPath in test.

--- test/lib.helpers.js
+++ test/lib.helpers.js
@@ -2,6 +2,7 @@ var test = require('tape');
 var helpers = require('../lib/helpers.js');
 
 test('helpers.getFieldByPath', t => {
+	var fieldByPath = helpers.getFieldByPath;
 	var complexObj = {
 		name: {
 			first: 'Big',
@@ -25,32 +26,30 @@ test('helpers.getFieldByPath', t => {
 		}
 	};
 
-	t.doesNotThrow(helpers.getFieldByPath, 'does not throw without arguments');
-	t.doesNotThrow(() => helpers.getFieldByPath(null, 'foo'), 'does not throw without an object');
-	t.doesNotThrow(() => helpers.getFieldByPath(complexObj), 'does not throw without a path');
+	t.doesNotThrow(fieldByPath, 'does not throw without arguments');
+	t.doesNotThrow(() => fieldByPath(null, 'foo'), 'does not throw without an object');
+	t.doesNotThrow(() => fieldByPath(complexObj), 'does not throw without a path');
 
-	t.deepEqual(helpers.getFieldByPath(complexObj), complexObj, 'no path returns the original object');
-	t.deepEqual(helpers.getFieldByPath(complexObj, ''), complexObj, 'empty path returns the original object');
+	t.deepEqual(fieldByPath(complexObj), complexObj, 'no path returns the original object');
+	t.deepEqual(fieldByPath(complexObj, ''), complexObj, 'empty path returns the original object');
 
-	t.equal(helpers.getFieldByPath(complexObj, 'invalid'), null, 'invalid path returns null');
-	t.equal(helpers.getFieldByPath(complexObj, 'foo.bar'), null, 'invalid long path returns null');
+	t.equal(fieldByPath(complexObj, 'invalid'), null, 'invalid path returns null');
+	t.equal(fieldByPath(complexObj, 'foo.bar'), null, 'invalid long path returns null');
 
-	t.deepEqual(
-		helpers.getFieldByPath(complexObj, 'name'), {first: 'Big', last: 'Guy'}, 'valid path short returns the inner object'
-	);
-	t.equal(helpers.getFieldByPath(complexObj, 'name.first'), 'Big', 'valid long path returns the string value');
-	t.equal(helpers.getFieldByPath(complexObj, 'location.state.full'), 'Washington', 'valid long path returns the string value');
+	t.deepEqual(fieldByPath(complexObj, 'name'), {first: 'Big', last: 'Guy'}, 'valid path short returns the inner object');
+	t.equal(fieldByPath(complexObj, 'name.first'), 'Big', 'valid long path returns the string value');
+	t.equal(fieldByPath(complexObj, 'location.state.full'), 'Washington', 'valid long path returns the string value');
 
-	t.equal(helpers.getFieldByPath(complexObj, 'name.last.toString'), null, 'returns null if path is not an object');
-	t.equal(helpers.getFieldByPath(complexObj, 'status.age.toString'), null, 'returns null if path is not an object');
-	t.equal(helpers.getFieldByPath(complexObj, 'status.married.toString'), null, 'returns null if path is not an object');
-	t.equal(helpers.getFieldByPath(complexObj, 'status.single.toString'), null, 'returns null if path is not an object');
+	t.equal(fieldByPath(complexObj, 'name.last.toString'), null, 'returns null if path is not an object');
+	t.equal(fieldByPath(complexObj, 'status.age.toString'), null, 'returns null if path is not an object');
+	t.equal(fieldByPath(complexObj, 'status.married.toString'), null, 'returns null if path is not an object');
+	t.equal(fieldByPath(complexObj, 'status.single.toString'), null, 'returns null if path is not an object');
 
-	t.equal(helpers.getFieldByPath(complexObj, 'status.age'), 10, 'able to return a number');
-	t.equal(helpers.getFieldByPath(complexObj, 'status.children'), 0, 'able to return a falsy number');
-	t.equal(helpers.getFieldByPath(complexObj, 'status.married'), false, 'able to return false');
-	t.equal(helpers.getFieldByPath(complexObj, 'status.single'), true, 'able to return true');
-	t.equal(helpers.getFieldByPath(complexObj, 'status.spouse'), null, 'able to return null');
+	t.equal(fieldByPath(complexObj, 'status.age'), 10, 'able to return a number');
+	t.equal(fieldByPath(complexObj, 'status.children'), 0, 'able to return a falsy number');
+	t.equal(fieldByPath(complexObj, 'status.married'), false, 'able to return false');
+	t.equal(fieldByPath(complexObj, 'status.single'), true, 'able to return true');
+	t.equal(fieldByPath(complexObj, 'status.spouse'), null, 'able to return null');
 
 	t.end();
 });
